c384ad50d6c5a41036682739beaadd22e4f61eab
SLMeri <74334648+SLMeri@users.noreply.github.com>
2022-04-08 13:37:42 -0700

    save and load game, AB#529 AB#530

--- src/com/GameLogic/Game.java
+++ src/com/GameLogic/Game.java
@@ -15,11 +15,26 @@ import java.util.concurrent.TimeUnit;
 
 // Definition of what is a game
 public class Game implements Serializable{
-    private Player player = new Player();
+    private static Player player = new Player();
     final ScheduledExecutorService timer = Executors.newScheduledThreadPool(1);
+    final Runnable runnable = new Runnable() {
+        int countdownStarter = 5;
+        @Override
+        public void run() {
+            System.out.println(countdownStarter);
+            countdownStarter--;
+
+            if(countdownStarter < 1){
+                System.out.println("Restarting game now");
+                timer.shutdown();
+            }
+        }
+    };
     static Game game;
     Scanner in = new Scanner(System.in);
     ArrayList<Room> map = (ArrayList<Room>) ImportJSON.getMap();
+    //Object for timer on game restart -Meri
+
 
 
     // Not sure why this is relevant, will look into it -Meri
@@ -47,124 +62,7 @@ public class Game implements Serializable{
         this.player = player;
     }
 
-    //Refactored the following methods in order to separate concerns. Could be refactored more but this
-    // is a starting point - Meri
-    public void printSplashScreen() throws IOException, InterruptedException {
-        Printer.print(Story.beginGameText());
-    }
-
-    public void playerSetup(){
-        String name = in.nextLine();
-        Player user = new Player(name,110, 20);
-        setPlayer(user);
-        Player player = getPlayer();
-        player.setCurrentRoom(map.get(0));
-        System.out.println("Ok, " + user.getName() + " this isn't going to be an easy adventure are you ready? (Type yes or y)");
-    }
-
-    public void printPlayerDetails(){
-        System.out.println(getPlayer());
-    }
-
-    public boolean checkIfPlayerReady(){
-        String response = in.nextLine();
-        if(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")){
-            printPlayerDetails();
-           return true;
-        }
-        return false;
-    }
-    // Method for creating a game
-    public boolean beginGame() throws IOException, InterruptedException {
-        printSplashScreen();
-        playerSetup();
-        if (checkIfPlayerReady()){
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
-
-    //Object for timer on game restart -Meri
-    final Runnable runnable = new Runnable() {
-        int countdownStarter = 5;
-        @Override
-        public void run() {
-            System.out.println(countdownStarter);
-            countdownStarter--;
-
-            if(countdownStarter < 1){
-                System.out.println("Restarting game now");
-                timer.shutdown();
-            }
-        }
-    };
-
-    public void restartGame() throws InterruptedException, IOException {
-        System.out.println("Are you sure you want to restart");
-        String response = in.nextLine();
-        if(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")){
-            timer.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
-            Thread.sleep(6000);
-            beginGame();
-        }else {
-            System.out.println("Glad you chose to keep playing!");
-            Thread.sleep(2000);
-            System.out.println("Lets keep playing");
-            Thread.sleep(2000);
-        }
-
-    }
-
-    public static void saveGame(){
-        try{
-            FileOutputStream fos = new FileOutputStream("game.sav");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(game);
-            oos.flush();
-            oos.close();
-            System.out.println("Game saved");
-        }catch (Exception e){
-            System.out.println("Serialization Error! Can't save data.\n"
-                    + e.getClass() + ": " + e.getMessage() + "\n");
-        }
-    }
-
-    public static void loadGame(){
-        try{
-            FileInputStream fis = new FileInputStream("gave.sav");
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            game = (Game) ois.readObject();
-            ois.close();
-            System.out.println("Game loaded");
-        }catch (Exception e){
-            System.out.println("Serialization Error! Can't load data.\n");
-            System.out.println(e.getClass() + ": " + e.getMessage() + "\n");
-        }
-    }
-
-    public void showPlayerDetails(){
-        System.out.println("\n" + player.getName() + " is at the " + player.getCurrentRoomName());
-    }
-
-    public void showLocationDescription(){
-        System.out.println(player.showCurrentRoomDesc());
-    }
-
-    public void playerIntro(){
-        Player player = getPlayer();
-        player.setItems(player.getItems());
-        showPlayerDetails();
-        showLocationDescription();
-    }
-
-
-    //Method for running the game
-
-    public void playGame() throws IOException, ParseException, InterruptedException {
-        playerIntro();
+    public void gameEngine() throws IOException, InterruptedException, ParseException {
         Scanner in = new Scanner(System.in);
         Printer.print(Story.promptPlayerMessage());
         String[] location = in.nextLine().split(" ");
@@ -173,7 +71,7 @@ public class Game implements Serializable{
                 Printer.print(Story.quitMessage());
                 System.exit(130);
             } else if ("restart".equalsIgnoreCase(location[0]) || "r".equalsIgnoreCase(location[0])){
-              restartGame();
+                restartGame();
             }else if("save".equalsIgnoreCase(location[0])) {
                 saveGame();
             }else if("load".equalsIgnoreCase(location[0])){
@@ -204,7 +102,7 @@ public class Game implements Serializable{
             }
             else if ("equip".equalsIgnoreCase(location[0])) {
                 if(PlayerMechanics.checkInstance(getPlayer(),location[1])) {
-                PlayerMechanics.equipWeapon(getPlayer(),location[1]);
+                    PlayerMechanics.equipWeapon(getPlayer(),location[1]);
                 } else {
                     PlayerMechanics.equipArmor(getPlayer(),location[1]);
                 }
@@ -214,9 +112,126 @@ public class Game implements Serializable{
                 Printer.print(Story.invalidEntryMessage2());
                 playGame();
             }
-        } catch (IndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException | ParseException e) {
             playGame();
         }
     }
+
+    //Method for running the game
+
+    public void playGame() throws IOException, ParseException, InterruptedException {
+        playerIntro();
+        gameEngine();
+    }
+
+
+
+
+
+
+    //Refactored the following methods in order to separate concerns. Could be refactored more but this
+    // is a starting point - Meri
+    public void printSplashScreen() throws IOException, InterruptedException {
+        Printer.print(Story.beginGameText());
+    }
+
+    public void playerSetup(){
+        String name = in.nextLine();
+        Player user = new Player(name,110, 20);
+        setPlayer(user);
+        Player player = getPlayer();
+        player.setCurrentRoom(map.get(0));
+        System.out.println("Ok, " + user.getName() + " this isn't going to be an easy adventure are you ready? (Type yes or y)");
+    }
+
+    public void printPlayerDetails(){
+        System.out.println(getPlayer());
+    }
+
+
+
+
+
+    public void restartGame() throws InterruptedException, IOException {
+        System.out.println("Are you sure you want to restart");
+        String response = in.nextLine();
+        if(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")){
+            timer.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
+            Thread.sleep(6000);
+            beginGame();
+        }else {
+            System.out.println("Glad you chose to keep playing!");
+            Thread.sleep(2000);
+            System.out.println("Lets keep playing");
+            Thread.sleep(2000);
+        }
+
+    }
+
+    public static void saveGame(){
+        try{
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("savedGame"));
+            oos.writeObject(player);
+            oos.flush();
+            oos.close();
+            System.out.println("Game saved");
+        }catch (Exception e){
+            System.out.println("Serialization Error! Can't save data.\n"
+                    + e.getClass() + ": " + e.getMessage() + "\n");
+        }
+    }
+
+    public static void loadGame(){
+        try{
+            FileInputStream fis = new FileInputStream("savedGame");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            player = (Player) ois.readObject();
+            ois.close();
+            System.out.println("Game loaded");
+            System.out.println("Here is where you left off");
+            System.out.println(player.getCurrentRoom() + " " + player.getItems());
+        }catch (Exception e){
+            System.out.println("Serialization Error! Can't load data.\n");
+            System.out.println(e.getClass() + ": " + e.getMessage() + "\n");
+        }
+    }
+
+    public void showPlayerDetails(){
+        System.out.println("\n" + player.getName() + " is at the " + player.getCurrentRoomName());
+    }
+
+    public void showLocationDescription(){
+        System.out.println(player.showCurrentRoomDesc());
+    }
+
+    public void playerIntro(){
+        Player player = getPlayer();
+        player.setItems(player.getItems());
+        showPlayerDetails();
+        showLocationDescription();
+    }
+
+    public boolean checkIfPlayerReady(){
+        String response = in.nextLine();
+        if(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")){
+            printPlayerDetails();
+            return true;
+        }
+        return false;
+    }
+    // Method for creating a game
+    public boolean beginGame() throws IOException, InterruptedException {
+        printSplashScreen();
+        playerSetup();
+        if (checkIfPlayerReady()){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
 
 }
