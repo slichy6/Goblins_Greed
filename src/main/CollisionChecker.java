9067471bcc0e6b0acb4774243a38828062187e59
slichy6 <94019317+slichy6@users.noreply.github.com>
2022-04-14 14:56:17 -0500

    added object pickup and console display for item categories

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -64,56 +64,77 @@ public class CollisionChecker {
         }
     }
 
-    // public int checkObject(Characters characters, boolean player){
+     public int checkObject(Characters characters, boolean player){
 
-    //     int index = 999;
+         int index = 999;
 
-    //     for(int i = 0; i < gp.obj.length; i++){
-    //         if(gp.obj[i] != null){
+         for(int i = 0; i < gp.obj.length; i++){
+             if(gp.obj[i] != null){
 
-    //             //get player's solid area position
-    //             characters.solidArea.x = characters.worldX + characters.solidAreaDefaultX;
-    //             characters.solidArea.y = characters.worldY + characters.solidAreaDefaultY;
-    //             //get object's solid area position
-    //             gp.obj[i].solidArea.x = gp.obj[i].worldX + gp.obj[i].solidArea.x;
-    //             gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y;
+                 //get player's solid area position
+                 characters.solidArea.x = characters.worldX + characters.solidArea.x;
+                 characters.solidArea.y = characters.worldY + characters.solidArea.y;
 
-    //             switch(characters.direction){
-    //                 case "up":
-    //                     characters.solidArea.y -= characters.speed;
-    //                     if(characters.solidArea.intersects(gp.obj[i].solidArea)){
-    //                         System.out.print("up collision!");
-    //                     }
-    //                     break;
+                 //get object's solid area position
+                 gp.obj[i].solidArea.x = gp.obj[i].worldX + gp.obj[i].solidArea.x;
+                 gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y;
 
-    //                 case "down":
-    //                     characters.solidArea.y += characters.speed;
-    //                     if(characters.solidArea.intersects(gp.obj[i].solidArea)){
-    //                         System.out.print("down collision!");
-    //                     }
-    //                     break;
+                 switch(characters.direction){
+                     case "up":
+                         characters.solidArea.y -= characters.speed;
+                         if(characters.solidArea.intersects(gp.obj[i].solidArea)){
+                             if(gp.obj[i].collision){
+                                 characters.collisionOn = true;
+                             }
+                             if(player == true){
+                                 index = i;
+                             }
+                         }
+                         break;
 
-    //                 case "right":
-    //                     characters.solidArea.x += characters.speed; 
-    //                     if(characters.solidArea.intersects(gp.obj[i].solidArea)){
-    //                         System.out.print("right collision!");
-    //                     }
-    //                     break;
+                     case "down":
+                         characters.solidArea.y += characters.speed;
+                         if(characters.solidArea.intersects(gp.obj[i].solidArea)){
+                             if(gp.obj[i].collision){
+                                 characters.collisionOn = true;
+                             }
+                             if(player == true){
+                                 index = i;
+                             }
+                         }
+                         break;
 
-    //                 case "left":    
-    //                     characters.solidArea.x -= characters.speed;
-    //                     if(characters.solidArea.intersects(gp.obj[i].solidArea)){
-    //                         System.out.print("left collision!");
-    //                     }
-    //                     break;
-    //             }
-    //             characters.solidArea.x = characters.solidAreaDefaultX;
-    //             characters.solidArea.y = characters.solidAreaDefaultY;
-    //             gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
-    //             gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
-    //         }
-    //     }
+                     case "right":
+                         characters.solidArea.x += characters.speed;
+                         if(characters.solidArea.intersects(gp.obj[i].solidArea)){
+                             if(gp.obj[i].collision){
+                                 characters.collisionOn = true;
+                             }
+                             if(player == true){
+                                 index = i;
+                             }
+                         }
+                         break;
 
-    //     return index;
-    // }
+                     case "left":
+                         characters.solidArea.x -= characters.speed;
+                         if(characters.solidArea.intersects(gp.obj[i].solidArea)){
+                             if(gp.obj[i].collision){
+                                 characters.collisionOn = true;
+                             }
+                             if(player == true){
+                                 index = i;
+                             }
+                         }
+                         break;
+                 }
+                 characters.solidArea.x = characters.solidAreaDefaultX;
+                 characters.solidArea.y = characters.solidAreaDefaultY;
+                 gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
+                 gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
+             }
+         }
+
+         return index;
+     }
 }
